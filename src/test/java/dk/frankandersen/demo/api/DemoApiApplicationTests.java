37c3d76f80214472d4a7a6c091c362484365a34d
Frank Andersen <mail@frankandersen.dk>
2022-02-07 14:07:43 +0100

    first, springboot start

---
+++ src/test/java/dk/frankandersen/demo/api/DemoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.frankandersen.demo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
