b01998cb53f3f7704ecf7e30de8d34d8f20b0b79
Frank Andersen <mail@frankandersen.dk>
2022-02-09 08:40:16 +0100

    working as example

---
+++ src/main/java/dk/frankandersen/demo/service/PersonService.java
@@ -0,0 +1,42 @@
+package dk.frankandersen.demo.service;
+
+import dk.frankandersen.demo.api.Person;
+import dk.frankandersen.demo.dao.PersonDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class PersonService {
+
+    private final PersonDao personDao;
+
+    @Autowired
+    public PersonService(@Qualifier("fakeDao") PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    public int addPerson(Person person) {
+        return personDao.insertPerson(person);
+    }
+
+    public List<Person> getAllPeople() {
+        return personDao.selectAllPeople();
+    }
+
+    public Optional<Person> getPersonById(UUID id) {
+        return  personDao.selectPersonById(id);
+    }
+
+    public int deletePerson(UUID id) {
+        return personDao.deletePersonById(id);
+    }
+
+    public int updatePerson(UUID id, Person newPerson) {
+        return personDao.updatePersonById(id, newPerson);
+    }
+}
